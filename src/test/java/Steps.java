7c0bb19164e003c6fd2457e821901bd398b593f9
venc47 <venc47@sg.uob>
2019-09-15 12:33:02 +0800

    PSD Training

--- src/test/java/Steps.java
+++ src/test/java/Steps.java
@@ -15,7 +15,7 @@ public class Steps {
     @When("^click \"([^\"]*)\"$")
     public void click(String arg1) throws Throwable {
         System.out.print("result is"+ new RpcCalc().compute("2 3 +"));
-        Assert.assertEquals(5,new RpcCalc().compute("10 12 /"));
+        Assert.assertEquals(0,new RpcCalc().compute("10 12 /"));
         //Assert.assertEquals(-5,new RpcCalc().compute("2 2 -12 + /"));
         // Write code here that turns the phrase above into concrete actions
         //throw new PendingException();
